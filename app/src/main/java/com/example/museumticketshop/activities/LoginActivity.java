5f41172879e3f4058cd654207132e542e141fb9e
Ábel Bakk <abelbakk@gmail.com>
2023-04-22 16:15:12 +0200

    update existing views

--- app/src/main/java/com/example/museumticketshop/activities/LoginActivity.java
+++ app/src/main/java/com/example/museumticketshop/activities/LoginActivity.java
@@ -1,26 +1,44 @@
-package com.example.museumticketshop;
+package com.example.museumticketshop.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.museumticketshop.R;
+
+import java.util.Objects;
+
 public class LoginActivity extends AppCompatActivity {
-
     private static final Long SECRET_KEY = 852195325434L;
-
+    private static final String PREF_KEY = Objects.requireNonNull(LoginActivity.class.getPackage(),
+                    "Error happened during loading package name for LoginActivity")
+            .toString();
     private EditText emailAddressET;
     private EditText passwordET;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        sharedPreferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
         emailAddressET = findViewById(R.id.loginEmailAddress);
         passwordET = findViewById(R.id.loginPassword);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("loginEmailAddress", emailAddressET.getText().toString());
+        editor.apply();
     }
 
     public void login(View view) {
---
+++ app/src/main/java/com/example/museumticketshop/activities/RegisterActivity.java
@@ -0,0 +1,76 @@
+package com.example.museumticketshop.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.museumticketshop.R;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String PREF_KEY = Objects.requireNonNull(LoginActivity.class.getPackage(),
+                    "Error happened during loading package name for RegisterActivity")
+            .toString();
+    private static final String VALUE_DOES_NOT_EXIST = "default value";
+    private EditText firstNameET;
+    private EditText lastNameET;
+    private EditText emailAddressET;
+    private EditText streetAndNumberET;
+    private EditText cityET;
+    private EditText postalCodeET;
+    private EditText passwordET;
+    private EditText rePasswordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        if (!Objects.equals(getIntent().getLongExtra("secretKey", 0L),
+                852195325434L)) {
+            finish();
+        }
+
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
+        if (!Objects.equals(
+                sharedPreferences.getString("loginEmailAddress", VALUE_DOES_NOT_EXIST),
+                VALUE_DOES_NOT_EXIST)) {
+            emailAddressET.setText
+                    (sharedPreferences.getString("loginEmailAddress", VALUE_DOES_NOT_EXIST));
+        }
+
+        firstNameET = findViewById(R.id.registerFirstName);
+        lastNameET = findViewById(R.id.registerLastName);
+        emailAddressET = findViewById(R.id.registerEmailAddress);
+        streetAndNumberET = findViewById(R.id.registerStreetAndNumber);
+        cityET = findViewById(R.id.registerCity);
+        postalCodeET = findViewById(R.id.registerPostalCode);
+        passwordET = findViewById(R.id.registerPassword);
+        rePasswordET = findViewById(R.id.registerRePassword);
+    }
+
+    public void register(View view) {
+        String firstName = extractStringFromEditText(firstNameET);
+        String lastName = extractStringFromEditText(lastNameET);
+        String emailAddress = extractStringFromEditText(emailAddressET);
+        String streetAndNumber = extractStringFromEditText(streetAndNumberET);
+        String city = extractStringFromEditText(cityET);
+        String postalCode = extractStringFromEditText(postalCodeET);
+        String rawPassword = extractStringFromEditText(passwordET);
+        String rawRePassword = extractStringFromEditText(rePasswordET);
+    }
+
+    public void redirectToLogin(View view) {
+        // TODO: might need to change to intent
+        finish();
+    }
+
+    private String extractStringFromEditText(EditText editText) {
+        return editText == null ? "" : editText.getText().toString();
+    }
+}
